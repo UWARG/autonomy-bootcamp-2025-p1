75f4aebb429ecd2dd943527301d1bfca3453eca1
Abdullah Khalid <abdullahkhalid@eduroam-campus-10-36-45-175.campus-dynamic.uwaterloo.ca>
2025-10-09 19:11:58 -0400

    removed unused variables

--- modules/detect_colours.py
+++ modules/detect_colours.py
@@ -54,9 +54,6 @@ class DetectBlue:
         # Apply the threshold for the colour detection
         mask = cv2.inRange(hsv, lower_blue, upper_blue)
 
-        # Shows the detected colour from the mask
-        res = cv2.bitwise_and(img, img, mask=mask)
-
         # ============
         # ↑ BOOTCAMPERS MODIFY ABOVE THIS COMMENT ↑
         # ============
@@ -121,9 +118,6 @@ class DetectRed:
         mask_one = cv2.inRange(hsv, lower_red_one, upper_red_one)
         mask_two = cv2.inRange(hsv, lower_red_two, upper_red_two)
         mask = cv2.bitwise_or(mask_one, mask_two)
-
-        # Shows the detected colour from the mask
-        res = cv2.bitwise_and(img, img, mask=mask)
 
         # Annotate the colour detections
         # replace the '_' parameter with the appropiate variable

8be5ce17654107c6c92402a8c093fe7f08d60dd2
aryannkapoorr29 <akk926836@gmail.com>
2026-05-14 20:47:36 -0400

    Format with black

--- modules/detect_colours.py
+++ modules/detect_colours.py
@@ -45,17 +45,17 @@ class DetectBlue:
         # ============
 
         # Convert the image's colour to HSV
-        hsv = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+        hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
         # Set upper and lower bounds for colour detection, this is in HSV
-        lower_blue = np.array([100,150,50])
-        upper_blue = np.array([150,255,255])
+        lower_blue = np.array([100, 150, 50])
+        upper_blue = np.array([150, 255, 255])
 
         # Apply the threshold for the colour detection
-        mask = cv2.inRange(hsv,lower_blue,upper_blue)
+        mask = cv2.inRange(hsv, lower_blue, upper_blue)
 
         # Shows the detected colour from the mask
-        res = cv2.bitwise_and(img, img, mask = mask)
+        res = cv2.bitwise_and(img, img, mask=mask)
 
         # ============
         # ↑ BOOTCAMPERS MODIFY ABOVE THIS COMMENT ↑
@@ -114,18 +114,18 @@ class DetectRed:
         hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
         # Set upper and lower bounds for colour detection, this is in HSV
-        lower_red1 = np.array([0 ,150, 50])
-        upper_red1 = np.array([10 ,255 ,255])
-        mask1 = cv2.inRange(hsv,lower_red1,upper_red1)
-        lower_red2 = np.array([170 ,150, 50])
-        upper_red2 = np.array([179 ,255 ,255])
+        lower_red1 = np.array([0, 150, 50])
+        upper_red1 = np.array([10, 255, 255])
+        mask1 = cv2.inRange(hsv, lower_red1, upper_red1)
+        lower_red2 = np.array([170, 150, 50])
+        upper_red2 = np.array([179, 255, 255])
         mask2 = cv2.inRange(hsv, lower_red2, upper_red2)
 
         # Apply the threshold for the colour detection
         mask = cv2.bitwise_or(mask1, mask2)
 
         # Shows the detected colour from the mask
-        res = cv2.bitwise_and(img, img, mask = mask)
+        res = cv2.bitwise_and(img, img, mask=mask)
 
         # Annotate the colour detections
         # replace the '_' parameter with the appropiate variable
